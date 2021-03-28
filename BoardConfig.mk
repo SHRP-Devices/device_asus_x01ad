a92d01f453fbd0131b4d388815244849f3259e71
SP-XD <somnatypaul007@gmail.com>
2021-03-28 19:42:41 +0530

    X01AD: initial SHRP bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,7 @@ TW_HAS_EDL_MODE := true
 TW_USE_TOOLBOX := true
 TW_SKIP_COMPATIBILITY_CHECK := true
 
+
 # Debug
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
@@ -128,3 +129,34 @@ TW_INCLUDE_FUSE_NTFS := true
 
 #Ignore Missing Dependencies
 ALLOW_MISSING_DEPENDENCIES=true
+
+
+#SHRP VARIABLES 
+# Path of your SHRP Tree
+SHRP_PATH := device/asus/X01AD
+# Maintainer name *
+SHRP_MAINTAINER := SP-XD
+# Device codename *
+SHRP_DEVICE_CODE := X01AD
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := SAR
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A_Only
+
+SHRP_STATUSBAR_LEFT_PADDING := 40
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_NOTCH := true
+SHRP_DARK := true
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+SHRP_FLASH := 1
+SHRP_CUSTOM_FLASHLIGHT := true
+SHRP_FONP_1 := /sys/class/leds/led:torch_1/brightness
+SHRP_FONP_2 := /sys/class/leds/led:torch_0/brightness
+SHRP_FONP_3 := /sys/class/leds/led:switch_1/brightness
+SHRP_FLASH_MAX_BRIGHTNESS := 200
+SHRP_EDL_MODE := 1
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+INC_IN_REC_MAGISK := true
+
--- omni_X01AD.mk
+++ omni_X01AD.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
